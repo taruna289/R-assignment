c06785c3a8bfe15d2028fdfb52056ed7e697bc8e
Taruna <taruna289@gmail.com>
2020-04-17 20:24:06 +0530

    First commit

---
+++ R assignment 2_taruna.R
@@ -0,0 +1,54 @@
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+## Caching the Inverse of a Matrix
+
+makeCacheMat <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(inverse) inv <<- inverse
+  getInv <- function() inv
+  list(set = set,
+       get = get,
+       setInv = setInv,
+       getInv = getInv)
+}
+
+## This function computes the inverse
+cacheSolve <- function(x, ...) {
+  inv <- x$getInv()
+  if (!is.null(inv)) {
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInv(inv)
+  inv
+}
